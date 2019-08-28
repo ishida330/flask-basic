119060fa5f5fa147edafd25c290dc1a4aba10075
ipeuser22 <ibmpe.handson+22@gmail.com>
2019-08-28 11:10:31 +0900

    v4

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
-#import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
-#port = os.getenv('PORT', '5000') # portはIBM Cloud環境から割り当てられたものを利用
 
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=int(port), debug=True) # Procfile内でpython hello.pyで起動
-    app.run(host='0.0.0.0', port=8090)
+    app.run(host='0.0.0.0', port=8080)
