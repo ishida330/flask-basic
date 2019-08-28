86b004bfa946282f2187e783a644c7e0d0180091
ipeuser22 <ibmpe.handson+22@gmail.com>
2019-08-28 10:59:03 +0900

    v2

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def index():
 
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=int(port), debug=True) # Procfile内でpython hello.pyで起動
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
